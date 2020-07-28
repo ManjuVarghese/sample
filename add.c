bcabfdc9579f25d23e32dd9c5129bceb176fba1e
ManjuVarghese <manjuvarghese94@gmail.com>
2020-07-28 20:47:52 +0530

    new commit

---
+++ add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+   
+ int a, b, sum;
+   
+ printf("\nEnter two numbers: \n");
+  
+ scanf("%d %d", &a, &b);
+ 
+ sum = a + b;
+  
+ printf("Sum : %d", sum);
+  
+ return 0;
+
+}
